0010857539c37e40043d603630d56cabe8d7037b
Matthew Goodall <dogsaw@thecyberplains.com>
2014-02-09 10:35:08 +1300

    Format source with gofmt

--- db.go
+++ db.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-    "database/sql"
-    _ "github.com/mattn/go-sqlite3"
-    "log"
-    "strconv"
-    "time"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"strconv"
+	"time"
 )
 
-var schema = []string {
-    // v1-v3
-    // needs to be split into separate statements due to https://github.com/mattn/go-sqlite3/issues/60
-    // otherwise only the first statement will execute
-`
+var schema = []string{
+	// v1-v3
+	// needs to be split into separate statements due to https://github.com/mattn/go-sqlite3/issues/60
+	// otherwise only the first statement will execute
+	`
 CREATE TABLE config (
     key varchar PRIMARY KEY,
     value varchar
 );
 `,
-`
+	`
 CREATE TABLE documents (
     id integer PRIMARY KEY,
     path string
     );
 
 `,
-`
+	`
 CREATE TABLE word_count (
     id integer PRIMARY KEY,
     path string,
@@ -36,57 +36,60 @@ CREATE TABLE word_count (
 `,
 }
 
-type SchemaVersionNotFound struct {}
+type SchemaVersionNotFound struct{}
+
 func (s SchemaVersionNotFound) Error() string {
-    return "no schema version found"
+	return "no schema version found"
 }
 
-func openDb(dsn string) (db *sql.DB, err error){
-    db,err = sql.Open("sqlite3", dsn)
-    return
+func openDb(dsn string) (db *sql.DB, err error) {
+	db, err = sql.Open("sqlite3", dsn)
+	return
 }
 
 func getSchemaVersion(db *sql.DB) (version int, err error) {
 
-    row := db.QueryRow("pragma schema_version")
+	row := db.QueryRow("pragma schema_version")
 
-    var schema_version string
-    err = row.Scan(&schema_version)
-    if err != nil {
-        err = SchemaVersionNotFound{}
-        return
-    }
+	var schema_version string
+	err = row.Scan(&schema_version)
+	if err != nil {
+		err = SchemaVersionNotFound{}
+		return
+	}
 
-    version,err = strconv.Atoi(schema_version)
-    return
+	version, err = strconv.Atoi(schema_version)
+	return
 }
 
 func updateSchema(db *sql.DB) (err error) {
-    version,err := getSchemaVersion(db)
-    if err != nil {
-        return
-    }
-    if version < len(schema) {
-        tx,err := db.Begin()
-        for _, cmd := range schema[version:] {
-            log.Print(cmd)
-            _,err =  db.Exec(cmd)
-            if err != nil {
-                _ = tx.Rollback()
-                return err
-            }
-        }
-        err = tx.Commit()
-    }
+	version, err := getSchemaVersion(db)
+	if err != nil {
+		return
+	}
+	if version < len(schema) {
+		tx, err := db.Begin()
+		for _, cmd := range schema[version:] {
+			log.Print(cmd)
+			_, err = db.Exec(cmd)
+			if err != nil {
+				_ = tx.Rollback()
+				return err
+			}
+		}
+		err = tx.Commit()
+	}
 
-    if (err == nil){
-        _,err =  db.Exec("pragma schema_version = " + strconv.Itoa(len(schema)))
-    }
-    return
+	if err == nil {
+		log.Println(strconv.Itoa(len(schema)))
+		println(2)
+		_, err = db.Exec("pragma schema_version = " + strconv.Itoa(len(schema)))
+	}
+	return
 }
 
 func getPreviousDayWordCount(db *sql.DB, path string) (count int, err error) {
-    cmd := `
+	cmd := `
 SELECT words
 FROM word_count
 WHERE path = ?
@@ -95,55 +98,55 @@ ORDER BY timestamp DESC
 LIMIT 1,1
 `
 
-    y,m,d := time.Now().UTC().Date()
-    today := time.Date(y,m,d,0,0,0,0, time.UTC)
-    row := db.QueryRow(cmd, path, today)
-    var wordsColumn string
-    err = row.Scan(&wordsColumn)
-    if (err == sql.ErrNoRows) {
-        return 0, nil
-    } else if (err != nil){
-        return
-    }
-    if (wordsColumn == ""){
-        return 0, nil
-    } else {
-        count,err = strconv.Atoi(wordsColumn)
-        return
-    }
+	y, m, d := time.Now().UTC().Date()
+	today := time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+	row := db.QueryRow(cmd, path, today)
+	var wordsColumn string
+	err = row.Scan(&wordsColumn)
+	if err == sql.ErrNoRows {
+		return 0, nil
+	} else if err != nil {
+		return
+	}
+	if wordsColumn == "" {
+		return 0, nil
+	} else {
+		count, err = strconv.Atoi(wordsColumn)
+		return
+	}
 }
 
-func addWordCount(db *sql.DB, path string, count int) (err error){
-    cmd := `
+func addWordCount(db *sql.DB, path string, count int) (err error) {
+	cmd := `
 INSERT INTO word_count (path, words, timestamp)
 VALUES (?, ?, ?)
 `
 
-    time := time.Now().UTC().Unix()
-    _,err = db.Exec(cmd, path, count, time)
-    return
+	time := time.Now().UTC().Unix()
+	_, err = db.Exec(cmd, path, count, time)
+	return
 }
 
-func getPreviousWordCount(db *sql.DB, path string) (count int, err error){
-    cmd := `
+func getPreviousWordCount(db *sql.DB, path string) (count int, err error) {
+	cmd := `
 SELECT words
 FROM word_count
 WHERE path = ?
 ORDER BY timestamp DESC
 LIMIT 1,1
 `
-    row := db.QueryRow(cmd, path)
-    var wordsColumn string
-    err = row.Scan(&wordsColumn)
-    if (err == sql.ErrNoRows) {
-        return 0, nil
-    } else if (err != nil){
-        return
-    }
-    if (wordsColumn == ""){
-        return 0, nil
-    } else {
-        count,err = strconv.Atoi(wordsColumn)
-        return
-    }
+	row := db.QueryRow(cmd, path)
+	var wordsColumn string
+	err = row.Scan(&wordsColumn)
+	if err == sql.ErrNoRows {
+		return 0, nil
+	} else if err != nil {
+		return
+	}
+	if wordsColumn == "" {
+		return 0, nil
+	} else {
+		count, err = strconv.Atoi(wordsColumn)
+		return
+	}
 }
--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"database/sql"
 	"flag"
 	"fmt"
-	"database/sql"
+	"log"
 	"os"
 	"path/filepath"
-	"bufio"
-	"log"
 	"regexp"
+	"strconv"
 	"strings"
-    "strconv"
 )
 
 type Document struct {
-	Path string
-	Words int
-	Prev int
+	Path      string
+	Words     int
+	Prev      int
 	Yesterday int
 }
 type DocumentMap map[string]Document
@@ -27,20 +27,19 @@ func (document *Document) Today() int {
 }
 
 var (
-	documentBase = "."
-	documentPath = []string{"."}
-	databasePath = "./wc.db"
-	headerFormat = "Total: #{total} Today: #{today}#{goal}"
-	goalFormat = " Goal: #{target}(#{remaining})"
-	itemFormat = "#{path}: #{total} (#{today})"
-	annotationPattern = `#.*$`
-	goal = 0
+	documentBase       = "."
+	documentPath       = []string{"."}
+	databasePath       = "./wc.db"
+	headerFormat       = "Total: #{total} Today: #{today}#{goal}"
+	goalFormat         = " Goal: #{target}(#{remaining})"
+	itemFormat         = "#{path}: #{total} (#{today})"
+	annotationPattern  = `#.*$`
+	goal               = 0
 	extensionBlacklist = []string{}
 	extensionWhitelist = []string{}
 )
 
-
-func init(){
+func init() {
 	flag.StringVar(&documentBase, "base", documentBase, "TODO")
 	flag.StringVar(&documentBase, "b", documentBase, "TODO")
 
@@ -58,9 +57,9 @@ func init(){
 
 }
 
-func countAll(documents string, base string, db *sql.DB, annotationRegexp *regexp.Regexp, files DocumentMap) error{
+func countAll(documents string, base string, db *sql.DB, annotationRegexp *regexp.Regexp, files DocumentMap) error {
 	return filepath.Walk(documents, func(path string, info os.FileInfo, _ error) error {
-		if (info.IsDir()){
+		if info.IsDir() {
 			return nil
 		}
 		countFile(path, base, db, annotationRegexp, files)
@@ -70,25 +69,33 @@ func countAll(documents string, base string, db *sql.DB, annotationRegexp *regex
 
 func countFile(path string, base string, db *sql.DB, annotationRegexp *regexp.Regexp, files DocumentMap) (err error) {
 	words := countWords(path, annotationRegexp)
-	prev,err := getPreviousWordCount(db, path)
-	if (err != nil){ return }
-	yesterday,err := getPreviousDayWordCount(db, path)
-	if (err != nil){ return }
+	prev, err := getPreviousWordCount(db, path)
+	if err != nil {
+		return
+	}
+	yesterday, err := getPreviousDayWordCount(db, path)
+	if err != nil {
+		return
+	}
 
-	abs_path,err := filepath.Abs(path)
-	if (err != nil){ return }
+	abs_path, err := filepath.Abs(path)
+	if err != nil {
+		return
+	}
 
-	rel_path,err := filepath.Rel(base, abs_path)
-	if (err != nil){ return }
+	rel_path, err := filepath.Rel(base, abs_path)
+	if err != nil {
+		return
+	}
 
-	if (words != prev){
+	if words != prev {
 		addWordCount(db, rel_path, words)
 	}
-	files[rel_path] = Document { Path: rel_path, Words: words, Prev: prev, Yesterday: yesterday }
+	files[rel_path] = Document{Path: rel_path, Words: words, Prev: prev, Yesterday: yesterday}
 	return
 }
 
-func countWords(path string, annotationRegexp *regexp.Regexp) int{
+func countWords(path string, annotationRegexp *regexp.Regexp) int {
 	file, err := os.Open(path)
 	defer file.Close()
 	if err != nil {
@@ -104,37 +111,39 @@ func countWords(path string, annotationRegexp *regexp.Regexp) int{
 		count += len(strings.Fields(line))
 	}
 	if err := scanner.Err(); err != nil {
-		log.Println("error reading ", path , ":", err)
+		log.Println("error reading ", path, ":", err)
 	}
 	return count
 }
 
 func main() {
 	flag.Parse()
-	args := flag.Args();
+	args := flag.Args()
 	if len(args) > 0 {
 		documentPath = args
 	}
 
-	annotationRegexp,err := regexp.Compile(annotationPattern)
-	if err != nil {log.Fatal("Bad annotation pattern")}
+	annotationRegexp, err := regexp.Compile(annotationPattern)
+	if err != nil {
+		log.Fatal("Bad annotation pattern")
+	}
 
-	basePath,_ := filepath.Abs(filepath.Dir(databasePath))
+	basePath, _ := filepath.Abs(filepath.Dir(databasePath))
 
-	db,err := openDb(databasePath)
-	if(err != nil){
+	db, err := openDb(databasePath)
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	err = updateSchema(db)
-	if(err != nil){
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	var files = make(DocumentMap)
-	for _,path := range documentPath {
-		fileMode,err := os.Stat(path)
+	for _, path := range documentPath {
+		fileMode, err := os.Stat(path)
 		if err != nil {
 			log.Println(err)
 		} else if fileMode.IsDir() {
@@ -147,7 +156,7 @@ func main() {
 	if headerFormat != "" {
 		today := 0
 		total := 0
-		for _,document := range(files) {
+		for _, document := range files {
 			today += document.Today()
 			total += document.Words
 		}
@@ -155,10 +164,10 @@ func main() {
 		headerOutput = strings.Replace(headerOutput, "#{total}", strconv.Itoa(total), -1)
 		headerOutput = strings.Replace(headerOutput, "#{today}", strconv.Itoa(today), -1)
 
-		if (goal > 0){
+		if goal > 0 {
 			goalOutput := goalFormat
 			goalOutput = strings.Replace(goalOutput, "#{target}", strconv.Itoa(goal), -1)
-			goalOutput = strings.Replace(goalOutput, "#{remaining}", strconv.Itoa(goal - today), -1)
+			goalOutput = strings.Replace(goalOutput, "#{remaining}", strconv.Itoa(goal-today), -1)
 			headerOutput = strings.Replace(headerOutput, "#{goal}", goalOutput, -1)
 		} else {
 			headerOutput = strings.Replace(headerOutput, "#{goal}", "", -1)
@@ -167,7 +176,7 @@ func main() {
 	}
 
 	if itemFormat != "" {
-		for _,document := range(files) {
+		for _, document := range files {
 			itemOutput := itemFormat
 			itemOutput = strings.Replace(itemOutput, "#{path}", document.Path, -1)
 			itemOutput = strings.Replace(itemOutput, "#{total}", strconv.Itoa(document.Words), -1)
